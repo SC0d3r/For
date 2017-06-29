71e23231367a7c27a26a50da006dea87ef3c860a
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:18:16 +0430

    add all factories to For class

--- src/for.ts
+++ src/for.ts
@@ -1,7 +1,18 @@
+import { ReturnsOfCounter } from './returnsOfCounterClass';
+import { TillFactory } from './tillFactory.class';
+import { ReturnFactory } from './returnFactory.class';
 import { DoFactory } from './doFactory.class';
-import { Mixed, COUNTER } from "./types";
+import { Mixed, COUNTER, RETURN, EnteringTillFactory, TILLS } from "./types";
 
-export function For(mixed: Mixed) : COUNTER{
-    var doFactory = new DoFactory(mixed);
-    return doFactory.getCounter();
+
+
+type ForReturns = COUNTER & RETURN & TILLS;
+
+export function For(mixed: Mixed) : ForReturns{
+    const counter = (new DoFactory(mixed)).getCounter();
+    const returns = (new ReturnFactory(<string[] | number[]>mixed)).getReturn();
+    const tills = (new TillFactory(<EnteringTillFactory>mixed)).getTills();
+    const returnMethods =  (new ReturnsOfCounter<any>(<any>mixed)).getReturns();
+    const combinedFactories : ForReturns = Object.assign({} , counter , returns , tills , returnMethods);
+    return combinedFactories;
 }
