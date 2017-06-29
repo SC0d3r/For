5955d251d4c5ff67c7557a9f5c059e268d14a9ae
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:17:40 +0430

    comment bundling in index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Mixed, COUNTER } from './types';
 import { For } from "./for";
-interface MyWindow extends Window {
-    For : (mixed : Mixed) => COUNTER;
-}
-(<MyWindow>window).For = For;
+
+// for using in browser uncomment this and run > npm run bundle
+// interface MyWindow extends Window {
+//     For : (mixed : Mixed) => COUNTER;
+// }
+// (<MyWindow>window).For = For;
+
