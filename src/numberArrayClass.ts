17f7df4046fb59b6696fafaf6c235d25ad56cdb7
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:17:23 +0430

    add returns to numberArray class

--- src/numberArrayClass.ts
+++ src/numberArrayClass.ts
@@ -1,4 +1,5 @@
-import { Counter, COUNTER } from './types';
+import { ReturnsOfCounter } from './returnsOfCounterClass';
+import { Counter, COUNTER, Returns } from './types';
 export class NumberArrayClass implements Counter {
     private copyNums: number[];
     constructor(nums: number[]) {
@@ -7,10 +8,14 @@ export class NumberArrayClass implements Counter {
 
     getCounter(): COUNTER {
         return {
-            do: (cb: (currentElement: number, index: number, arr: number[]) => void) => {
+            do: (cb?: (index: number, currentElement: number, arr: number[]) => number) => {
+                let changesOnArray : number[] = this.copyNums;
                 for (var i = 0; i < this.copyNums.length; i++) {
-                    cb(this.copyNums[i], i, this.copyNums);
+                    let ret = cb && cb(i, this.copyNums[i], this.copyNums);
+                    if(typeof ret === 'number') changesOnArray[i] = ret;
+                    else changesOnArray[i] = ret || changesOnArray[i];
                 }
+                return (new ReturnsOfCounter<number>(changesOnArray)).getReturns();
             }
         }
     }
