eb0c0b0858412ff2b0ca81440b025376626d0b68
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:15:38 +0430

    add returnUniq class

---
+++ src/returnUniqClass.ts
@@ -0,0 +1,22 @@
+import { Return ,  RETUNIQ} from './types';
+export class ReturnUniqClass implements Return{
+    constructor(private array : string[] | number[]){}
+
+    getReturn() : RETUNIQ {
+        let array = this.array;
+        const length = array.length;
+        let uniqArr : any = [];
+
+        return {
+            returnUniq : () => {
+                for (let i = 0; i < length; i++) {
+                    let currentItem = array[i];
+                    if(uniqArr.indexOf(currentItem) === -1){
+                        uniqArr.push(currentItem);
+                    }
+                }
+                return uniqArr;
+            }
+        }
+    }
+}
