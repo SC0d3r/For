912b3f00f25aa47643020a519dea288ce7eadedc
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:14:13 +0430

    add more types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,53 @@
-export type Mixed = number | number[] | string | string[] | Object;
+export type Mixed = number | number[] | string | string[] | Object | Object[];
 
-export type COUNTER  = {do : Function};
-export type RETURN = {returnSum : Function};
+// -------- return of counters
+export type Returns = {
+    returns: any[];
+    assign: (array: any[]) => void;
+    append: (array: any[]) => void;
+    prepend: (array: any[]) => void;
+}
+// -------- counters
+export type COUNTER = { do: (key?: any, value?: any, copyOfArr?: any[]) => Returns };
 
 export interface Counter {
-    getCounter() : COUNTER;
+    getCounter(): COUNTER;
 }
+
+
+// ---------- returns
+export type RETSUM = {
+    returnSum: () => string | number;
+}
+
+export type RETUNIQ = {
+    returnUniq: () => number[] | string[];
+}
+
+export type RETSORTED = {
+    returnSorted: (cb?: (a: any, b: any) => number) => number[] | string[];
+}
+
+export type RETURN = RETSUM & RETUNIQ & RETSORTED;
+
+export interface Return {
+    getReturn(): RETSUM | RETUNIQ | RETSORTED;
+}
+
+
+// ---------- tills
+export type EnteringTillFactory = string[] | number[] | Object[] | Object;
+
+export type TillKey = {
+    tillKey: (condition: string) => COUNTER & Returns;
+}
+
+export type TillValue = {
+    tillValue: (condition: string) => COUNTER & Returns;
+}
+
+export interface Tills {
+    getTills(): TillKey | TillValue;
+}
+
+export type TILLS = TillKey & TillValue;
