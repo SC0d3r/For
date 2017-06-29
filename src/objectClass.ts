2ffc3aff84c589032a15b87dee2d925a3732154e
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:17:04 +0430

    add returns to ObjectClass

--- src/objectClass.ts
+++ src/objectClass.ts
@@ -1,17 +1,24 @@
+import { ReturnsOfCounter } from './returnsOfCounterClass';
 import { Counter, COUNTER } from './types';
 export class ObjectClass implements Counter {
     constructor(private obj: any) { }
 
     getCounter(): COUNTER {
         return {
-            do: (cb: (currentKey : string , currentValue : any) => void) => {
+            do: (cb?: (currentKey: string, currentValue: any) => [string , any]) => {
                 let keys = Object.keys(this.obj);
                 let length = keys.length;
+                let changedArray : [string , any][] = [];
+
                 for (var i = 0; i < length; i++) {
-                    let currentKey : string = keys[i];
-                    let currentValue : string = this.obj[currentKey];
-                    cb(currentKey , currentValue);
+                    let currentKey: string = keys[i];
+                    let currentValue: string = this.obj[currentKey];
+                    changedArray[i] = [currentKey , currentValue];
+                    let ret = cb && cb(currentKey, currentValue);
+                    changedArray[i] = ret || changedArray[i];
                 }
+                
+                return (new ReturnsOfCounter<[string , any]>(changedArray)).getReturns();
             }
         }
     }
