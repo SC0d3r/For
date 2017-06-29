44c80ad8dd74561f45f14b18574cb7e031588875
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:16:02 +0430

    add returnSorted class

---
+++ src/returnSortedClass.ts
@@ -0,0 +1,17 @@
+import { Return ,  RETSORTED} from './types';
+
+export class ReturnSortedClass implements Return{
+    private sorted : string[] | number[];
+    constructor(private array : string[] | number[]){}
+
+    getReturn() : RETSORTED {
+        
+        let self = this;
+        return {
+            returnSorted : (cb ?: (a : any , b : any) => number) => {
+                let copyArray = self.array.slice();
+                return (<number[]>copyArray).sort(cb);
+            }
+        }
+    }
+}
