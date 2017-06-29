1224fbc7d654041cc91a0bf109c8ad31776da9a6
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:14:25 +0430

    add tillValue class

---
+++ src/tillValueClass.ts
@@ -0,0 +1,69 @@
+import { ReturnsOfCounter } from './returnsOfCounterClass';
+import { Tills, EnteringTillFactory, TillValue, COUNTER, Returns } from './types';
+import { DoFactory } from './doFactory.class';
+
+export class TillValueClass implements Tills {
+    constructor(private mixed: EnteringTillFactory) { }
+
+    getTills(): TillValue {
+        return {
+            tillValue: (condition: string): COUNTER & Returns => {
+                let condtionedArrayOrObj: any[] | Object = this.mixed;
+                if (Array.isArray(this.mixed)) condtionedArrayOrObj = this.mixed.slice();
+
+                let counterForThisType: COUNTER = (new DoFactory(this.mixed)).getCounter();
+                // console.log(counterForThisType);
+                counterForThisType.do((key: (number | string), val: any) => {
+                    const conditionResult: boolean = this.parseConditionOnThisVal(condition, val);
+                    if (!Boolean(conditionResult)) delete condtionedArrayOrObj[key];
+                });
+                if (Array.isArray(condtionedArrayOrObj)) condtionedArrayOrObj = removeUndefinedsFromArray(condtionedArrayOrObj);
+                let returns =  (new ReturnsOfCounter<any>(<any>condtionedArrayOrObj)).getReturns();
+                let counter =  (new DoFactory(condtionedArrayOrObj)).getCounter();
+                return Object.assign({} , returns , counter);
+            }
+        }
+    }
+
+    private parseConditionOnThisVal(condition: string, onWhat: string | number): boolean {
+        let cond : string, value : string | number, keyInObj : string = '';
+        [cond, value] = condition.trim().split(' ');
+        if(typeof this.mixed[0] === 'object') [keyInObj , cond , value] = condition.trim().split(' ');
+        let val: any = parseInt(value);
+        if (isNaN(val)) {
+            val = value;
+        }
+
+        switch (cond) {
+            case "<":
+                if ((onWhat[keyInObj] || onWhat) < val) return true;
+                else return false;
+            case ">":
+                if ((onWhat[keyInObj] || onWhat) > val) return true;
+                else return false;
+            case "<=":
+                if ((onWhat[keyInObj] || onWhat) <= val) return true;
+                else return false;
+            case ">=":
+                if ((onWhat[keyInObj] || onWhat) >= val) return true;
+                else return false;
+            case "=":
+                // if (typeof (onWhat[keyInObj] || onWhat) === 'string') val = val + '';
+                if ((onWhat[keyInObj] || onWhat) === val) return true;
+                else return false;
+            default:
+                throw new Error(`the condtion you passed is incorrect \n
+                * if this is an object the first thing in condition string must be the desured field in object *`);
+        }
+    }
+}
+
+function removeUndefinedsFromArray(array: any[]): any[] {
+    let length = array.length;
+    let resultArr: any[] = [];
+    for (let i = 0; i < length; i++) {
+        let currentElem = array[i]
+        if (currentElem !== undefined) resultArr.push(currentElem);
+    }
+    return resultArr;
+}
