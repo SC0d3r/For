07c1fef0b2c92e2e6fb375bb88b7931563014ee5
SC0d3r <sasan.kelishani@gmail.com>
2017-06-29 23:17:13 +0430

    add returns to numberClass

--- src/numberClass.ts
+++ src/numberClass.ts
@@ -1,12 +1,18 @@
+import { ReturnsOfCounter } from './returnsOfCounterClass';
 import { Counter, COUNTER } from './types';
 export class NumberClass implements Counter {
     constructor(private counter: number) { }
     getCounter(): COUNTER {
         return {
-            do: (cb: (counter: number) => void) => {
-                for (var i = 0; i < this.counter; i++) {
-                    cb(i);
+            do: (cb?: (index: number) => number) => {
+                let indexes: number[] = [];
+                for (let ind = 0; ind < this.counter; ind++) {
+                    indexes[ind] = ind;
+                    let ret = cb && cb(ind);
+                    if(typeof ret === 'number') indexes[ind] = ret;
+                    else indexes[ind] = ret || indexes[ind];
                 }
+                return (new ReturnsOfCounter<number>(indexes)).getReturns();
             }
         }
     }
