4648ccfeeabb487ea4f818f3c40e89177bbc77af
SC0d3r <sasan.kelishani@gmail.com>
2017-07-02 01:27:48 +0430

    remove copying of array in stringArrayClass

--- src/doFactory/stringArrayClass.ts
+++ src/doFactory/stringArrayClass.ts
@@ -1,17 +1,16 @@
 import { ReturnsOfCounter } from './../returnsOfCounterClass';
 import { Counter, COUNTER } from './../interfaces/types';
 export class StringArrayClass implements Counter {
-    private copyOfArray: string[];
-    constructor(private strArray: string[]) { this.copyOfArray = this.strArray.slice(); }
+    constructor(private strArray: string[]) {  }
 
     getCounter(): COUNTER {
         let length = this.strArray.length;
         return {
-            do: (cb?: (index: number, currentElement: string, copyOfArray: string[]) => string) => {
-                let changedArray : string[] = this.copyOfArray;
+            do: (cb?: (index: number, currentElement: string, strArray: string[]) => string) => {
+                let changedArray : string[] = this.strArray;
 
                 for (var i = 0; i < length; i++) {
-                    let ret = cb && cb(i, this.copyOfArray[i], this.copyOfArray);
+                    let ret = cb && cb(i, this.strArray[i], this.strArray);
                     changedArray[i] = ret || changedArray[i];
                 }
                 return (new ReturnsOfCounter<string>(changedArray)).getReturns();
