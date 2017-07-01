fb839a2486a8e9970a853df9148b7dcd8486df52
SC0d3r <sasan.kelishani@gmail.com>
2017-07-02 03:06:17 +0430

    refactor and add setMixedObj to rreturnFactoryClass

--- src/returnFactory/returnFactory.class.ts
+++ src/returnFactory/returnFactory.class.ts
@@ -6,15 +6,22 @@ import { Return, RETURN } from "../interfaces/types";
 
 export class ReturnFactory implements Return {
     private factoryReturn : RETURN;
-    constructor(private mixedType: string[] | number[]) {
-        this.makeReturn();
+    constructor(private mixedType ?: string[] | number[]) {
+        mixedType ? this.makeReturn() : '';
     }
+
     private makeReturn() : void {
-        const factoryReturnUniq = (new ReturnUniqClass(this.mixedType)).getReturn();
-        const factoryReturnSum = (new ReturnSumClass(this.mixedType)).getReturn();
-        const factoryReturnSorted = (new ReturnSortedClass(this.mixedType)).getReturn();
+        const factoryReturnUniq = (new ReturnUniqClass(<string[] | number[]>this.mixedType)).getReturn();
+        const factoryReturnSum = (new ReturnSumClass(<string[] | number[]>this.mixedType)).getReturn();
+        const factoryReturnSorted = (new ReturnSortedClass(<string[] | number[]>this.mixedType)).getReturn();
 
         this.factoryReturn = Object.assign({} , factoryReturnSum , factoryReturnUniq , factoryReturnSorted);
+    }
+
+    public setMixedObj(mixed : string[] | number[]) : ReturnFactory{
+        this.mixedType = mixed;
+        this.makeReturn();
+        return this;
     }
 
     getReturn(): RETURN {
