88460e35046945b396b29115926d2aaf777e7453
SC0d3r <sasan.kelishani@gmail.com>
2017-07-02 01:26:29 +0430

    add capability of disabaling the copy of an given array

--- src/for.ts
+++ src/for.ts
@@ -7,13 +7,38 @@ import { DoFactory } from './doFactory/doFactory.class';
 import { COUNTER, RETURN, TILLS, Mixed, EnteringTillFactory } from "./interfaces/types";
 
 type ForReturns = COUNTER & RETURN & TILLS;
+type FOR_FUNC = (Mixed) => COUNTER & RETURN & TILLS;
 
-function For(mixed: Mixed) : ForReturns{
-    const counter = (new DoFactory(mixed)).getCounter();
-    const returns = (new ReturnFactory(<string[] | number[]>mixed)).getReturn();
-    const tills = (new TillFactory(<EnteringTillFactory>mixed)).getTills();
-    const returnMethods =  (new ReturnsOfCounter<any>(<any>mixed)).getReturns();
-    const combinedFactories : ForReturns = Object.assign({} , counter , returns , tills , returnMethods);
+let For: any;
+
+For = (mixed: Mixed): ForReturns => {
+    let copyOfMixed: any[] = [];
+    if (For.__copy && Array.isArray(mixed)) copyOfMixed = returnCopyOfGivenArray(mixed);
+    else copyOfMixed = <any>mixed;
+
+    const counter = (new DoFactory(copyOfMixed)).getCounter();
+    const returns = (new ReturnFactory(<string[] | number[]>copyOfMixed)).getReturn();
+    const tills = (new TillFactory(<EnteringTillFactory>copyOfMixed)).getTills();
+    const returnMethods = (new ReturnsOfCounter<any>(<any>copyOfMixed)).getReturns();
+    const combinedFactories: ForReturns = Object.assign({}, counter, returns, tills, returnMethods);
     return combinedFactories;
 }
+
+For.__copy = true; // by default all the given arrays will be copied and used 
+
+// if you diable copying the returnSorted method will copy either way
+For.disableCopy = () => {
+    For.__copy = false;
+}
+For.enableCopy = () => {
+    For.__copy = true;
+}
+
+function returnCopyOfGivenArray(arr : any[]){
+    return arr.slice();
+}
+
+
+
+
 export = For;
