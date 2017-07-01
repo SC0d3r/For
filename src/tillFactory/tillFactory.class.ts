ceba515c46243bf1b7937231be082ff889f8c0de
SC0d3r <sasan.kelishani@gmail.com>
2017-07-02 03:06:35 +0430

    refactor and add setMixedObj to tillFactoryClass

--- src/tillFactory/tillFactory.class.ts
+++ src/tillFactory/tillFactory.class.ts
@@ -4,15 +4,27 @@ import { TillKeyClass } from './tillKeyClass';
 import { Tills, EnteringTillFactory, TILLS } from "../interfaces/types";
 
 
-export class TillFactory implements Tills{
-    constructor(private mixed: EnteringTillFactory) {
+export class TillFactory implements Tills {
+    private tills : TILLS;
+    constructor(private mixed ?: EnteringTillFactory) {
+        mixed ? this.createTills() : '';
     };
 
     getTills(): TILLS {
-        let tillKey = (new TillKeyClass(this.mixed)).getTills();
-        let tillValue = (new TillValueClass(this.mixed)).getTills();
-        let tills : TILLS = Object.assign({} , tillKey , tillValue);
-        return tills;
+        return this.tills;
     }
 
+
+    public setMixedObj(mixed: EnteringTillFactory) : TillFactory {
+        this.mixed = mixed;
+        this.createTills();
+        return this;
+    }
+
+    private createTills(){
+        let tillKey = (new TillKeyClass(<EnteringTillFactory>this.mixed)).getTills();
+        let tillValue = (new TillValueClass(<EnteringTillFactory>this.mixed)).getTills();
+        let tills: TILLS = Object.assign({}, tillKey, tillValue);
+        this.tills = tills;
+    }
 }
