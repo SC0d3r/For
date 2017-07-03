5100eba77b7b40c0edc3c7e25a20e82bbdb1e0db
SC0d3r <sasan.kelishani@gmail.com>
2017-07-04 02:16:00 +0430

    add public method setMixedObj to stringClass

--- src/doFactory/stringClass.ts
+++ src/doFactory/stringClass.ts
@@ -1,14 +1,15 @@
 import { ReturnsOfCounter } from './../returnsOfCounterClass';
 import { Counter, COUNTER } from "../interfaces/types";
 
-export class StringClass implements Counter {
-    constructor(private str: string) { }
+export class StringClass implements Counter<StringClass> {
+    
+    constructor(private _str ?: string) { }
 
     getCounter(): COUNTER {
         return {
             do: (cb?: (index: number, currentChar: string, str: string) => string) => {
                 let changedArray : string[] = [];
-                let str = this.str;
+                let str = <string>this._str;
                 for (var i = 0; i < str.length; i++) {
                     let ret = cb && cb(i, str[i], str);
                     changedArray[i] = ret || str[i];
@@ -16,6 +17,11 @@ export class StringClass implements Counter {
                 return (new ReturnsOfCounter<string>(changedArray)).getReturns();
             }
         }
+    }
+
+    setMixedObj(mixed: string): StringClass {
+        this._str = mixed;
+        return this;
     }
 
 }
