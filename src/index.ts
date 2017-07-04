556556c3f79dff258fd61d00276255cf10ef3e02
SC0d3r <sasan.kelishani@gmail.com>
2017-07-05 01:38:59 +0430

    fix index.ts to bundle and use For in browser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-// import { Mixed, COUNTER } from './types';
-// import For = require('./for');
+import For = require('./for');
+import { Mixed, COUNTER } from "./interfaces/types";
 
 // for using in browser uncomment bellow and run > npm run bundle
 
-// interface MyWindow extends Window {
-//     For : (mixed : Mixed) => COUNTER;
-// }
-// (<MyWindow>window).For = For;
+interface MyWindow extends Window {
+    For : (mixed : Mixed) => COUNTER;
+}
+(<MyWindow>window).For = For;
 
